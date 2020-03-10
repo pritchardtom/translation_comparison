3a2bdfe451f35a3db9115b931b53b809fb7331a9
Tom Pritchard <>
2020-03-10 18:09:55 +0000

    Added file translation.

--- README.md
+++ README.md
@@ -12,9 +12,9 @@ It will also perform metrics on the returned translations, BLEU, for example.
 ## To-Do
 
 - Add function for dealing with input_file i.e., save file to pandas df, batch the translations, and save them to df.
-        - Already written, just need to tweak to include in translator.py
+  - Already written, just need to tweak to include in translator.py
 - Add BLEU scores, and check API docs as some may provide this already.
-        - Although, a few seem to produce a variant of this, i.e., confidence in the translation itself.
+  - Although, a few seem to produce a variant of this, i.e., confidence in the translation itself.
 - The single_sentence_translate function should return a dictionary of the translations, rather than print them?
 - Modify some of the args to be optional/required etc.  Check argparse doc for params.
 - Add date_time stamp to file outputs to avoid creating duplicates/overwriting.
--- translator.py
+++ translator.py
@@ -60,18 +60,37 @@ def single_sentence_translate(text, source_language, target_language):
     yandex = yandex_translate(text, source_language, target_language)
     microsoft = microsoft_translate(text, source_language, target_language)
     google = google_translate(text, source_language, target_language)
-    print(yandex)
-    print(microsoft)
-    print(google)
+    print(f"Yandex Translation:    {yandex}")
+    print(f"Microsoft Translation: {microsoft}")
+    print(f"Google Translation:    {google}")
+
+
+def file_translate(dataframe, source_language, target_language):
+    yandex_trans = []
+    microsoft_trans = []
+    google_trans = []
+    for item in dataframe["Source"]:
+        yandex_trans.append(yandex_translate(item, source_language, target_language))
+        microsoft_trans.append(microsoft_translate(item, source_language, target_language))
+        google_trans.append(google_translate(item, source_language, target_language))
+    dataframe["Yandex_Translation"] = yandex_trans
+    dataframe["Microsoft_Translation"] = microsoft_trans
+    dataframe["Google_Translation"] = google_trans
+    return dataframe
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--input_file", help="Select the .csv file with source sentences.")
 parser.add_argument("-o", "--output_file", help="Select a filename to save the results --> (not avail under '-s' option).")
-parser.add_argument("-p", "--phrase", help="Translate a single phrase/sentence.  Result returned to stdout.")
+parser.add_argument("-p", "--phrase", help="Translate a single phrase/sentence.  Result returned to stdout.", default="how the devil are you?")
 parser.add_argument("-s", "--source_lang", help="Source language of input text/phrase.", default="en")
 parser.add_argument("-t", "--trans_to", help="Language to translate source to.", default="fr")
 args = parser.parse_args()
 
-if args.phrase:
-    test = single_sentence_translate(args.phrase, args.source_lang, args.trans_to)
+# if args.phrase:
+#     test = single_sentence_translate(args.phrase, args.source_lang, args.trans_to)
+
+if args.input_file:
+    df = readfile(args.input_file)
+    res = file_translate(df, args.source_lang, args.trans_to)
+    writefile(df, args.output_file)
